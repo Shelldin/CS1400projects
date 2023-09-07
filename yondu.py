ff7c91f94ee06e4f2c354f06581d0a435b22ef5a
Shelldin <sheldon.bean@gmail.com>
2023-09-06 20:15:05 -0600

    added input fields and changed the formatting style to f-strings

--- yondu.py
+++ yondu.py
@@ -1,25 +1,30 @@
-# total crew number and units input
+# total crew number input
 pirates_string = "How many pirates:"
-pirates_int = 20
+print(pirates_string)
+crew_size = int(input(""))
 
+print('')
+
+# total units input
 units_string = "How many units:"
-units_int = 1000
+print(units_string)
+total_units = int(input(""))
 
 # Yondu give the crew 3 units a piece before divvying out the plunder
-crew_initial_share = 3 * (pirates_int - 2)
+crew_initial_share = 3 * (crew_size - 2)
 
 # Yondu takes 13% of the remaining amount
-yondu_first_cut = round((units_int - crew_initial_share) * 0.13, 2)
+yondu_first_share = round((total_units - crew_initial_share) * 0.13, 2)
 
 # Peter gets 11% of the amount remaining after Yondu's cut
-peter_first_cut = round((units_int - crew_initial_share - yondu_first_cut) * 0.11, 2)
+peter_first_share = round((total_units - crew_initial_share - yondu_first_share) * 0.11, 2)
 
 # The remaining amount after Yondu's and Peter's initial cuts is divided equal with everyone
-crew_share = round((units_int - crew_initial_share - yondu_first_cut - peter_first_cut) / pirates_int, 2)
+crew_share = round((total_units - crew_initial_share - yondu_first_share - peter_first_share) / crew_size, 2)
 
 # each crew member's share after everything has been divided after initial cuts
-yondu_total_share = yondu_first_cut + crew_share
-peter_total_share = peter_first_cut + crew_share
+yondu_total_share = yondu_first_share + crew_share
+peter_total_share = peter_first_share + crew_share
 other_crew_total_share = 3 + crew_share
 
 yondu_string = "Yondu's Share: "
@@ -27,10 +32,7 @@ peter_string = "Peter's Share: "
 crew_string = "Crew's share: "
 
 # print the number of crew and units, as well as the final shares that will be received
-print(pirates_string, '\n', pirates_int)
-print(units_string, '\n', units_int)
 print('')
-print(yondu_string + "{:,.2f}".format(yondu_total_share))
-print(peter_string + "{:,.2f}".format(peter_total_share))
-print(crew_string + "{:,.2f}".format(other_crew_total_share))
-
+print(f"Yondu's Share: {yondu_total_share :,.2f}")
+print(f"Peter's Share: {peter_total_share :,.2f}")
+print(f"Crew's Share: {other_crew_total_share:,.2f}")
