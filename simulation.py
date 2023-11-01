d155acc354c885804c15c8f611274373b7bb6011
Shelldin <sheldon.bean@gmail.com>
2023-11-01 01:18:46 -0600

    got the population equation working properly

--- simulation.py
+++ simulation.py
@@ -2,7 +2,7 @@ import argparse
 
 
 def check_initial_population_input(init_pop):
-    if init_pop is not float or init_pop < 0 or init_pop > 1:
+    if init_pop < 0 or init_pop > 1:
         raise ValueError("Please enter a decimal value between 0 and 1")
 
 
@@ -12,8 +12,19 @@ def check_growth_rate_input(growth_rate):
 
 
 def check_iterations_input(iterations):
-    if iterations is not int or iterations < 0:
+    if iterations < 0:
         raise ValueError("Please enter a whole number that is greater than 0")
+
+
+def pop_equation(init_pop, rate, iterations):
+    result = round(init_pop, 3)
+
+    for n in range(0, iterations):
+        if n < 1:
+            print(f"{n} {result:,.3f}")
+        else:
+            result = round(rate * result * (1 - result), 3)
+            print(f"{n} {result:,.3f}")
 
 
 def population_simulation():
@@ -21,7 +32,7 @@ def population_simulation():
         description="Get initial population, growth rate, iterations, and output file from command line")
 
     parser.add_argument('initial_population', type=float,
-                        help="Enter an initial population as a decimal percentage (between 0 and 1")
+                        help="Enter an initial population as a decimal percentage (between 0 and 1)")
 
     parser.add_argument('growth_rate', type=float,
                         help="Enter the growth rate as a number between 0 and 4")
@@ -38,7 +49,11 @@ def population_simulation():
     check_growth_rate_input(args.growth_rate)
     check_iterations_input(args.iterations)
 
+    pop_equation(args.initial_population, args.growth_rate, args.iterations)
+
 
 def main():
     population_simulation()
 
+
+main()
