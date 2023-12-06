03289e644da081d93c70335917f6e19f474bb687
Shelldin <sheldon.bean@gmail.com>
2023-12-06 02:19:40 -0700

    completed library project

--- hw_test.py
+++ hw_test.py
@@ -1,8 +1,7 @@
 longest_key = ""
 longest_value = ""
 
-test_dict = {"string key": "string value",
-             "string key 1": "string value 1", "string key 2": "string value 2", "key 3": "value 3"}
+test_dict = {3: "string value", 5: "string value 1", 4: "string value 2", 1: "value 3", 2: "ranasdasdasdasdasddom stuff"}
 
 for key, value in test_dict.items():
     if len(value) >= len(longest_value):
@@ -30,3 +29,39 @@ def get_average_length(sorted_dict):
 
 
 print(f"average length: {get_average_length(test_dict)}")
+
+test_output = open("test_output.txt", "w")
+test_output.write("writing some stuff 1\n")
+test_output.write("writing some stuff 2\n")
+
+
+def get_longest_line(sorted_dict):
+    # find the longest line and its line number
+    longest_line = ""
+    longest_line_number = 0
+
+    for number, text in sorted_dict.items():
+        if len(text) >= len(longest_line):
+            longest_line = text
+            longest_line_number = number
+
+    longest_line_and_number = (longest_line_number, longest_line)
+
+    return longest_line_and_number
+
+
+test_output.write(f"Longest line ({get_longest_line(test_dict)[0]}): {get_longest_line(test_dict)[1]}\n")
+test_output.write(f"Longest line ({get_longest_line(test_dict)[0]}): {get_longest_line(test_dict)[1]}\n")
+for number, text in test_dict.items():
+    test_output.write(f"{text}\n")
+
+
+def sort_dictionary(unsorted_dict):
+    # take dictionary and sort it
+    sorted_list = sorted(unsorted_dict.items())
+
+    sorted_dict = dict(sorted_list)
+
+    return sorted_dict
+
+print(sort_dictionary(test_dict))
--- library.py
+++ library.py
@@ -4,7 +4,7 @@ import argparse
 
 
 def file_text_to_list(file):
-    # Open file of female names  and load into a list
+    # Open file of book file and load into a list
     if os.path.isfile(file) is True:
         text_data_list = []
         text_data = open(file, "r", encoding="utf-8")
@@ -43,7 +43,9 @@ def convert_to_dictionary(text_list):
 
 def sort_dictionary(unsorted_dict):
     # take dictionary and sort it
-    sorted_dict = sorted(unsorted_dict)
+    sorted_list = sorted(unsorted_dict.items())
+
+    sorted_dict = dict(sorted_list)
 
     return sorted_dict
 
@@ -72,4 +74,41 @@ def get_average_length(sorted_dict):
         total_length += len(text)
         number_of_lines += 1
 
-    average_length = round(total_length/number_of_lines)
+    average_length = round(total_length / number_of_lines)
+
+    return average_length
+
+
+def write_output_file(file, sorted_dict):
+    # write a new file with required info and sorted lines
+    three_letter_code = get_three_letter_code(file)
+
+    output_file = open(f"{three_letter_code}_book.txt", "w")
+
+    output_file.write(f"{three_letter_code}\n")
+    output_file.write(f"Longest line ({get_longest_line(sorted_dict)[0]}): {get_longest_line(sorted_dict)[1]}\n")
+    output_file.write(f"Average length: {get_average_length(sorted_dict)}\n")
+    for number, text in sorted_dict.items():
+        output_file.write(f"{text}\n")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Open scrambled book file")
+
+    parser.add_argument('--file', type=str, required=True,
+                        help="Please provide a file name on the command line")
+
+    args = parser.parse_args()
+
+    initial_book_file = args.file
+
+    text_list = file_text_to_list(initial_book_file)
+    unsorted_dict = convert_to_dictionary(text_list)
+    sorted_dict = sort_dictionary(unsorted_dict)
+    write_output_file(initial_book_file, sorted_dict)
+
+
+# Execute main program
+if __name__ == "__main__":
+    main()
